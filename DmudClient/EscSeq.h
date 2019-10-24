dc82d3d9bf425b59d1f821d14b104317fa9a0834
Shreddan <40694103+Shreddan@users.noreply.github.com>
2019-10-24 18:37:33 +0100

    Defined ANSI ESC sequence options

---
+++ DmudClient/EscSeq.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#define StyleNormal 0
+#define StyleBold 1
+#define StyleFaint 2
+#define StyleItalic 3
+#define StyleUnderline 4
+#define StyleReverseVideo 7
+
+#define FGBlack 30
+#define FGRed 31
+#define FGGreen 32
+#define FGYellow 33
+#define FGBlue 34
+#define FGMagenta 35
+#define FGCyan 36
+#define FGWhite 37
+
+#define BGBlack 40
+#define BGRed 41
+#define BGGreen 42
+#define BGYellow 43
+#define BGBlue 44
+#define BGMagenta 45
+#define BGCyan 46
+#define BGWhite 47
+
+#define BrightFGBlack 90
+#define BrightFGRed 91
+#define BrightFGGreen 92
+#define BrightFGYellow 93
+#define BrightFGBlue 94
+#define BrightFGMagenta 95
+#define BrightFGCyan 96
+#define BrightFGWhite 97
+
+#define BrightBGBlack 100
+#define BrightBGRed 101
+#define BrightBGGreen 102
+#define BrightBGYellow 103
+#define BrightBGBlue 104
+#define BrightBGMagenta 105
+#define BrightBGCyan 106
+#define BrightBGWhite 107
